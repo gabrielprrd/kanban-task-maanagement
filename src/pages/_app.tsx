895340bc8ab04e61d30dc2a18f436ae9979e6fdd
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-03-22 18:18:53 +0100

    feat(frontend): add plus jakarta sans font family to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,19 @@ import CssBaseline from '@mui/material/CssBaseline'
 import type { AppProps } from 'next/app'
 import theme from '@/styles/theme'
 import Head from 'next/head'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <style jsx global>{`
+          html {
+            font-family: ${plusJakartaSans.style.fontFamily};
+          }
+        `}</style>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <ThemeProvider theme={theme}>
