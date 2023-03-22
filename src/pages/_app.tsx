e1fd0e55c5766153b430aa74728aa5d6605d4ec9
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-03-22 18:19:27 +0100

    feat(frontend): add chakra provider instead of material-ui provider at application entry point

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
-import { ThemeProvider } from '@mui/material'
-import CssBaseline from '@mui/material/CssBaseline'
 import type { AppProps } from 'next/app'
 import theme from '@/styles/theme'
 import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
@@ -18,10 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
         `}</style>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <ChakraProvider resetCSS theme={theme}>
         <Component {...pageProps} />
-      </ThemeProvider>
+      </ChakraProvider>
     </>
   )
 }
