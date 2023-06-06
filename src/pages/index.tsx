4607d1658f580c9f27c31c6587ad0de7477813b1
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-06 18:50:41 +0200

    feat: add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import Welcome from '@/components/Welcome'
+import DashboardLayout from '@/components/layouts/Dashboard'
+import { ReactElement } from 'react'
+import { GetServerSidePropsContext } from 'next'
+import { requireAuth } from '../utils'
+
 export default function Home() {
-  return <></>
+  return <Welcome />
+}
+
+Home.getLayout = function (page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  return requireAuth(ctx, (session) => ({ props: { session } }))
 }
