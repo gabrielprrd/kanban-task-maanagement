4dafdb27aafd23590df2bfdef3b778af5aecf221
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-06 18:55:41 +0200

    feat: add types for  page layouts and SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,42 @@ import type { AppProps } from 'next/app'
 import theme from '@/styles/theme'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Plus_Jakarta_Sans } from 'next/font/google'
-import DashboardLayout from '@/components/layouts/Dashboard'
+import { Plus_Jakarta_Sans } from '@next/font/google'
+import { api } from '@/utils/index'
+import { SessionProvider } from 'next-auth/react'
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react'
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
-export default function App({ Component, pageProps }: AppProps) {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <>
       <Head>
-        <style jsx global>{`
-          html {
-            font-family: ${plusJakartaSans.style.fontFamily};
-          }
-        `}</style>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
-      <ChakraProvider resetCSS theme={theme}>
-        <DashboardLayout>
-          <Component {...pageProps} />
-        </DashboardLayout>
-      </ChakraProvider>
+      <SessionProvider session={session}>
+        <ChakraProvider resetCSS theme={theme}>
+          <main className={plusJakartaSans.className}>
+            {getLayout(<Component {...pageProps} />)}
+          </main>
+        </ChakraProvider>
+      </SessionProvider>
     </>
   )
 }
+
+export default api.withTRPC(App)
